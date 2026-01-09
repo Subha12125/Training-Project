cbf52918addac475f5da87a1309dc03cca84d1e5
Subha12125 <subhabratapaul12125@gmail.com>
2026-01-09 10:42:57 +0530

    bycrypt-data

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,5 +1,9 @@
+const { pass } = require("three/tsl");
 const User = require("../models/user_model");
+const bcrypt = require("bcryptjs");
 
+
+//? home
 const home = async (req, res) => {
     try {
         res
@@ -11,6 +15,8 @@ const home = async (req, res) => {
 
     }
 }
+
+//? register
 const register = async (req, res) => {
     try {
         const {username, email, phone, password} = req.body;
@@ -19,8 +25,13 @@ const register = async (req, res) => {
             return res.status(400).json({message : "Email is already registered..."});
         }
         else{
-            User.create({username, email, phone, password});
-            return res.status(200).json({message : "User registered...."})
+            const saltRound = 10;;
+            const hash_password = await bcrypt.hash(password, saltRound);
+            const userData = await User.create({username, email, phone, password: hash_password});
+        
+            // User.create({username, email, phone, password});
+            // return res.status(200).json({message : "User registered...."})
+            return res.status(200).json({msg : userData});
         }
         
         res.status(200).json({message:req.body});
